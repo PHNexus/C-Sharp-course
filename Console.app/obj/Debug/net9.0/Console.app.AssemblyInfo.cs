48160d756275d7e1794c955e54d314a72cf6dee7
Nexus <104232421+PHNexus@users.noreply.github.com>
2025-01-22 01:29:35 -0300

    ATualization

--- Console.app/obj/Debug/net9.0/Console.app.AssemblyInfo.cs
+++ Console.app/obj/Debug/net9.0/Console.app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Console.app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4de327105b12c07004fe8e953e9485d04be962f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30c984b54b50d69060f2ae252578e62334f0c8e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Console.app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Console.app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
