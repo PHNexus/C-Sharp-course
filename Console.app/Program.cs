9371bfc66a3acf04ad93f2913e31578e145054ed
Nexus <104232421+PHNexus@users.noreply.github.com>
2025-03-07 02:44:23 -0300

    a

--- Console.app/Program.cs
+++ Console.app/Program.cs
@@ -7,9 +7,11 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Teste2");
-            
+            Console.Write("Hello World\n!"); // ira retornar Hello,World
+            Console.Write("");  // /n ira quebrar a linha agora || WriteLine ira escrever o comando na tela e quebrar a linha
+           // Console.Write("teste01");|| // ira retornar teste 01
+           // Console.ReadLine();      || // ira detectar oque o usuario,escreveu // ira esperar uma acao do usuario
+                                   //  \n   == e um caracter que ira quebrar a linha
         }
     }
 }
--- Console.app/obj/Debug/net9.0/Console.app.AssemblyInfo.cs
+++ Console.app/obj/Debug/net9.0/Console.app.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Console.app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6673ad9457b7f88b55361712ed46c316c6a8d255")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97a5144bba9ea0612d99cadad8ae075f73a2d288")]
 [assembly: System.Reflection.AssemblyProductAttribute("Console.app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Console.app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
