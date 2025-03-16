b7a597604f1f9d5907eb4f4294a50e88f70772d7
Nexus <104232421+PHNexus@users.noreply.github.com>
2025-03-16 08:48:00 -0300

    tipos| e variavel

--- Console.app/Program.cs
+++ Console.app/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using Microsoft.Win32;
 
 namespace ConsoleApp  
 {
@@ -7,11 +8,20 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello World\n!"); // ira retornar Hello,World
-            Console.Write("");  // /n ira quebrar a linha agora || WriteLine ira escrever o comando na tela e quebrar a linha
-           // Console.Write("teste01");|| // ira retornar teste 01
-           // Console.ReadLine();      || // ira detectar oque o usuario,escreveu // ira esperar uma acao do usuario
-                                   //  \n   == e um caracter que ira quebrar a linha
+
+
+            var texto = ("Hello,World"); // variavel
+
+            Console.WriteLine(texto);
+            Console.WriteLine(texto*5);
+
+
+
+            // /n ira quebrar a linha agora || WriteLine ira escrever o comando na tela e quebrar a linha
+            // Console.Write("teste01");|| // ira retornar teste 01
+            // Console.ReadLine();      || // ira detectar oque o usuario,escreveu // ira esperar uma acao do usuario
+            //Console.Write("Hello World\n!"); // ira retornar Hello,World
+           //   Console.Write(""); 
         }
     }
 }
