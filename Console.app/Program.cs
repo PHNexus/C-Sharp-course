d416083fde4c3baf5fd3e77ae0b258286d253c9c
Nexus <104232421+PHNexus@users.noreply.github.com>
2025-03-27 18:41:37 -0300

    limpei

--- Console.app/Program.cs
+++ Console.app/Program.cs
@@ -9,64 +9,9 @@ namespace ConsoleApp
     /// </summary>
     internal class Program
     {
-        /// <summary>
-        /// The Main
-        /// </summary>
-        /// <param name="args">The args<see cref="string[]"/></param>
         internal static void Main(string[] args)
         {
-            // Declarar Variavel, Sem Declarar o Tipo
-            var texto = ("Hello,World");
-            Console.WriteLine(texto);
-
-            // cor var, e uma maneira mais preguisosa de se Declarar
-
-            // Declaracao de variavel Com Tipo
-            int numero1 = 1; // Numero Inteiro
-            float numero2 = 15.6F; // Numero Real
-            double numero3 = 125.61; //Numero Real
-            bool numero4 = true; // ou falso
-            string texto2 = "texto"; // tipo texto entre aspas duplas
-            Console.WriteLine(numero1);
-            Console.WriteLine(numero2);
-            Console.WriteLine(numero3);
-            Console.WriteLine(numero4);
-            Console.WriteLine(texto2);
-            // soma com Numero Inteiro
-            int somas = 12;
-            int soma = 12 + somas;
-            Console.WriteLine(soma);
-
-            // soma com Numero Real
-            double somas1 = 24;
-            double soma1 = 24 + somas1;
-            Console.WriteLine(soma1);
-
-            // Outras Formas De Declarar Variaveis
-
-            // maneira mais avancada, Tipagem Dinamica
-
-            dynamic cor_favorita = "Vermelho";
-            Console.WriteLine(cor_favorita);
-
-            // com variavel dinamica posso alterar o tipo dela
-            cor_favorita = 212121;
-            Console.WriteLine(cor_favorita);
-
-            // comentario com multiplas linhas /* */
-
-            /*
-             Int    || 156 como 32334 Numero Inteiro
-             Float  || 15.6 como 234.123 Numero Real, Com Virgula ===> Double/ decimal
-             Bool   || Boleano || so pode ter 2 valores verdadeiro ou falso
-             String || tipo de dado 'Texto'
-             Char   || '' aspas Simples 1 unico caracter
-            */
-
-            Console.Write("teste01"); // ira retornar teste 01
-            Console.ReadLine(); // ira detectar oque o usuario,escreveu // ira esperar uma acao do usuario
-            Console.Write("Hello World\n!"); // ira retornar Hello,World
-            // Console.Write("");
+            Console.Write("Hello,World");
         }
     }
 }
